d9c7bda5c475713d4ea1378c37aef69243271e6b
sonwson <cason101105@gmail.co>
2026-04-15 00:33:58 +0700

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const env = require('./config/env');
 const logger = require('./config/logger');
 const { connectDatabase } = require('./config/db');
-
+app.use(cors());
 async function bootstrap() {
   try {
     await connectDatabase();
